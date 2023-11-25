c492047b4f08d4450c1b47c3329a512ca192c7a4
Igor Guimarães <igorgui.tec@gmail.com>
2023-11-25 19:18:40 -0300

    Feat: Correction

--- src/components/HomeContainer.tsx
+++ src/components/HomeContainer.tsx
@@ -1,17 +1,26 @@
 import { TransactionsBox } from "@/components";
 import { TransactionButtonBox } from "./TransactionButtonBox";
+import styled from "styled-components";
 
 
 export function HomeContainer() {
     const name = localStorage.getItem("name");
     return (
         <div className="container">
-            <div>
+            {/* <TopBoard> */}
             <h1>Olá, {name}</h1>
-            </div>
+            {/* <h1>log out</h1> */}
+            {/* </TopBoard> */}
             <TransactionsBox />
             <TransactionButtonBox />
         </div>
     );
 }
+
+// const TopBoard = styled.div`
+//     display: flex;
+//     justify-content: space-around;
+//     align-items: center;
+//     width: 60%;
+// `
 
